55af0b6a4c51d3b4eb0655c0e57d71d394f9d0a8
Rics <202380322@psu.palawan.edu.ph>
2025-09-11 01:37:13 +0800

    url config

--- projectsite/urls.py
+++ projectsite/urls.py
@@ -13,15 +13,21 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
 from orgstudent.views import HomePageView, OrganizationList
 
+def home(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('index.html', views.home),
+    path('index.html', home),
     path('organization_list/', OrganizationList.as_view(), name='organization-list'),
 ]
+def home(request):
+    return render(request, 'home.html')
